ff1fb3725872ed10c7d4f67ae0e9443c369cf237
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-28 16:32:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,8 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+loop do 
+  puts phrase = "Welcome to Flatiron School's Web Development Course!"
 
 end
 
