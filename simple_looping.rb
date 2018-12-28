7d4c3a33df9e40ba3600bb29a746a6302f48d641
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-28 16:45:19 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
-    puts phrase 
+    puts phrase
     if prase <= 7
       break
     end
